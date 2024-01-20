ea524fa260dd39474cd69085b38293e71d359e44
YannosK <giannos2405@gmail.com>
2024-01-20 16:00:07 +0200

    I will test IntegerNodes with scaffold

--- IntegerNodes.c
+++ IntegerNodes.c
@@ -1,1 +1,36 @@
+#include <stdlib.h>
 #include "IntegerNodes.h"
+
+/****************************************************************************************************************************************************************/
+// STRUCT DEFINITIONS
+/****************************************************************************************************************************************************************/
+
+struct intStruct
+{
+    int intKeyValue;
+};
+
+/****************************************************************************************************************************************************************/
+// PUBLIC FUNCTIONS
+/****************************************************************************************************************************************************************/
+
+intKey int_createkey(int input_key)
+{
+    intKey key = (intKey)malloc(sizeof(struct intStruct));
+    key->intKeyValue = input_key;
+    return key;
+}
+
+size_t int_size(void)
+{
+    size_t intKeySize = sizeof(struct intStruct);
+    return intKeySize;
+}
+
+unsigned short int_compare(const intKey op1, const intKey op2)
+{
+    if (op1->intKeyValue > op2->intKeyValue)
+        return 1;
+    else
+        return 0;
+}
--- IntegerNodes.h
+++ IntegerNodes.h
@@ -1,4 +1,32 @@
 #ifndef IntegerNodes
 #define IntegerNodes
 
+/*
+    Pointer to a struct that contains a key, of type int
+*/
+typedef struct intStruct *intKey;
+
+/*
+    Creates a key of type int a returns a pointer to its struct
+*/
+intKey int_createkey(int input_key);
+
+/*
+    Returns the size of the integer key struct (basically the size of int key)
+*/
+size_t int_size(void);
+
+/*
+    A function that compares two integer keys
+
+    Arguments:
+    op1 : first opperand is a pointer to a struct of integer key
+    op2 : second opperand is a pointer to a struct of integer key
+
+    Returns:
+    1 if op1 > op2
+    0 if op2 >= op1
+*/
+unsigned short int_compare(const intKey op1, const intKey op2);
+
 #endif
